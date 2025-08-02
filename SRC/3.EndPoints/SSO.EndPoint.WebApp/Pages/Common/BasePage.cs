c038b69338ae41f773bb98e13475659919ae8526
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-02 23:20:42 +0330

    #Feature Add Common Pages

---
+++ SRC/3.EndPoints/SSO.EndPoint.WebApp/Pages/Common/BasePage.cs
@@ -0,0 +1,16 @@
+﻿namespace SSO.EndPoint.WebApp.Pages.Common;
+
+public abstract class BasePage : PageModel
+{
+}
+[Authorize]
+public abstract class AuthPage : BasePage
+{
+    protected AuthPage()
+    {
+        if (!User.Identity.IsAuthenticated)
+        {
+            Redirect("/Account/Login");
+        }
+    }
+}
