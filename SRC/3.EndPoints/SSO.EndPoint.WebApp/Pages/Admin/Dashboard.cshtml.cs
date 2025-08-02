2980842bc3e2a5fb5c4b8e6cb0bf24445631cead
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-02 23:20:26 +0330

    #Feature Add Admin Root

---
+++ SRC/3.EndPoints/SSO.EndPoint.WebApp/Pages/Admin/Dashboard.cshtml.cs
@@ -0,0 +1,8 @@
+namespace SSO.EndPoint.WebApp.Pages.Admin;
+
+public class DashboardModel : AuthPage
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ SRC/3.EndPoints/SSO.EndPoint.WebApp/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,8 @@
+namespace SSO.EndPoint.WebApp.Pages.Admin;
+
+public class IndexModel : AuthPage
+{
+    public void OnGet()
+    {
+    }
+}
